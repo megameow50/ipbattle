4417a8eabba9747cf03323f9eec7c722e9fc7dac
Simon <you@example.com>
2021-01-23 16:07:48 -0500

    update

--- js/ipbattle.js
+++ js/ipbattle.js
@@ -14,9 +14,9 @@ return this.name;
 }
 
 var bigList = [];
-bigList.push(newFighter("Darth Vader", "Star Wars"));
-bigList.push(newFighter("Darth Maul", "Star Wars"));
-bigList.push(newFighter("Dumbledore", "Harry Potter"));
-bigList.push(newFighter("Grindelwald", "Harry Potter"));
-bigList.push(newFighter("Zuko", "Avatar"));
-bigList.push(newFighter("Gandalf", "Lord Of The Rings"));
+bigList.push(new Fighter("Darth Vader", "Star Wars"));
+bigList.push(new Fighter("Darth Maul", "Star Wars"));
+bigList.push(new Fighter("Dumbledore", "Harry Potter"));
+bigList.push(new Fighter("Grindelwald", "Harry Potter"));
+bigList.push(new Fighter("Zuko", "Avatar"));
+bigList.push(new Fighter("Gandalf", "Lord Of The Rings"));
