4d388e95d585b561f6b1f4541bc90ac99f3fa00a
Simon <you@example.com>
2021-01-23 16:07:00 -0500

    update

--- js/ipbattle.js
+++ js/ipbattle.js
@@ -13,10 +13,10 @@ return this.name;
 
 }
 
-bigList = [];
-bigList.push(newFighter("Darth Vader", "Star Wars"))
-bigList.push(newFighter("Darth Maul", "Star Wars"))
-bigList.push(newFighter("Dumbledore", "Harry Potter"))
-bigList.push(newFighter("Grindelwald", "Harry Potter"))
-bigList.push(newFighter("Zuko", "Avatar"))
-bigList.push(newFighter("Gandalf", "Lord Of The Rings"))
+var bigList = [];
+bigList.push(newFighter("Darth Vader", "Star Wars"));
+bigList.push(newFighter("Darth Maul", "Star Wars"));
+bigList.push(newFighter("Dumbledore", "Harry Potter"));
+bigList.push(newFighter("Grindelwald", "Harry Potter"));
+bigList.push(newFighter("Zuko", "Avatar"));
+bigList.push(newFighter("Gandalf", "Lord Of The Rings"));
