085caeed9de26a4b8dffed2fdb088be2ebfc8960
Simon <you@example.com>
2021-01-23 16:00:55 -0500

    update

--- js/ipbattle.js
+++ js/ipbattle.js
@@ -1,1 +1,22 @@
 "use strict"
+"use strict"
+class Fighter {
+	constructor(n, c) {
+		this.name = n;
+		this.cat = c;
+	}
+
+toString() {
+return this.name;
+
+}
+
+}
+
+bigList = [];
+bigList.push(newFighter("Darth Vader", "Star Wars"))
+bigList.push(newFighter("Darth Maul", "Star Wars"))
+bigList.push(newFighter("Dumbledore", "Harry Potter"))
+bigList.push(newFighter("Grindelwald", "Harry Potter"))
+bigList.push(newFighter("Zuko", "Avatar"))
+bigList.push(newFighter("Gandalf", "Lord Of The Rings"))
